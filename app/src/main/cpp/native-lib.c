2012aba01b120f7f3026fa48ecf11b2bcbeaeaae
ssrlive <30760636+ssrlive@users.noreply.github.com>
2021-05-08 12:23:07 +0800

    Android Studio 4.2 happy.

--- app/src/main/cpp/native-lib.c
+++ app/src/main/cpp/native-lib.c
@@ -3,6 +3,7 @@
 
 extern JNIEXPORT jstring JNICALL
 Java_com_ssrlive_execnative_MainActivity_stringFromJNI(JNIEnv *env, jobject This) {
+    (void)This;
     // https://blog.csdn.net/u011068702/article/details/71425585
-    return (*env)->NewStringUTF(env, "Hello from C++");
+    return (*env)->NewStringUTF(env, "Hello from native code of C");
 }
